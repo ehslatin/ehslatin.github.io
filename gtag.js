50d16112c42bbecf9d9edf684e2c2481c9d2e08e
ehslatin <48776420+ehslatin@users.noreply.github.com>
2019-04-22 14:53:01 -0400

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,5 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138829963-1%22%3E</script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138829963-1</script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
